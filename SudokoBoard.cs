01debcb8c10ae07f94fc722f23cfe4cf0a76fa7a
alon0608 <alonsh0608@gmail.com>
2025-01-24 17:24:31 +0200

    added board

--- SudokoBoard.cs
+++ SudokoBoard.cs
@@ -6,7 +6,87 @@ using System.Threading.Tasks;
 
 namespace SudokuOmega7
 {
-    internal class SudokoBoard
+    public class SudokuBoard : IBoardSize
     {
+        private readonly int[,] _board;
+        private readonly int _size;
+        private readonly int _boxSize;
+
+        public SudokuBoard(string input)
+        {
+            _size = (int)Math.Sqrt(input.Length);
+            _boxSize = (int)Math.Sqrt(_size);
+
+            if (_size * _size != input.Length)
+                throw new ArgumentException("Invalid input length for Sudoku board.");
+
+            _board = new int[_size, _size];
+            InitializeBoard(input);
+        }
+
+        private void InitializeBoard(string input)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    char c = input[i * _size + j];
+                    _board[i, j] = c == '0' ? 0 : ConvertToNumber(c);
+                }
+            }
+        }
+
+        private int ConvertToNumber(char c)
+        {
+            if (char.IsDigit(c))
+                return c - '0';
+            if (char.IsLetter(c) && char.ToUpper(c) <= 'G')
+                return char.ToUpper(c) - 'A' + 10;
+
+            throw new ArgumentException($"Invalid character '{c}' in Sudoku input.");
+        }
+
+        public int GetSize()
+        {
+            return _size;
+        }
+
+        public int GetBoxSize()
+        {
+            return _boxSize;
+        }
+
+        public int GetCellValue(int row, int col)
+        {
+            return _board[row, col];
+        }
+
+        public void SetCellValue(int row, int col, int value)
+        {
+            _board[row, col] = value;
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    Console.Write(_board[i, j] == 0 ? '.' : ConvertToChar(_board[i, j]));
+                    if ((j + 1) % _boxSize == 0 && j + 1 != _size)
+                        Console.Write(" | ");
+                }
+                Console.WriteLine();
+                if ((i + 1) % _boxSize == 0 && i + 1 != _size)
+                    Console.WriteLine(new string('-', _size * 2));
+            }
+        }
+
+        private char ConvertToChar(int value)
+        {
+            if (value <= 9)
+                return (char)(value + '0');
+            return (char)((value - 10) + 'A');
+        }
     }
 }
