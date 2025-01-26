bc518cb4a022cf716458762cbec6caca5acfe6a2
alon0608 <alonsh0608@gmail.com>
2025-01-26 19:05:01 +0200

    added file input

--- FileInput.cs
+++ FileInput.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace SudokuOmega7
 {
-    internal class FileInput
+
+    public class FileInputSource : IInput
     {
+        private readonly string _filePath;
+
+        public FileInputSource(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public string GetInput()
+        {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException($"File not found: {_filePath}");
+
+            return File.ReadAllText(_filePath).Replace("\n", "").Replace("\r", "").Trim();
+        }
     }
+
 }
