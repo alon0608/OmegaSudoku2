3bb692e5bc193ca8f10ce2f00dadca3da042138e
alon0608 <alonsh0608@gmail.com>
2025-02-07 14:06:16 +0200

    recoverd game class

--- SudokuGame.cs
+++ SudokuGame.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using SudokuOmega7;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SudokuOmega7
+
+
+namespace ConsoleApp5
 {
-    internal class SudokuGame
+    public class SudokuGame
     {
+        private readonly SudokuBoard _board;
+        private readonly SudokuSolver _solver;
+
+        public SudokuGame(IInput inputSource)
+        {
+            string input = inputSource.GetInput();
+            _board = new SudokuBoard(input);
+            _solver = new SudokuSolver(_board);
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("first board:");
+            _board.PrintBoard();
+
+            if (!SudokuValidation.IsValid(_board))
+            {
+                Console.WriteLine("invalid board.");
+                return;
+            }
+
+            if (_solver.Solve())
+            {
+                Console.WriteLine("solution:");
+                _board.PrintBoard();
+            }
+            else
+            {
+                Console.WriteLine("no solution for this board.");
+            }
+        }
     }
 }
+
