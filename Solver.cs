03fc5adef7a4f2b9bc7357f13fa54465b9d7208b
alon0608 <alonsh0608@gmail.com>
2025-02-09 23:15:26 +0200

    changed solver

--- Solver.cs
+++ Solver.cs
@@ -1,162 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SudokuOmega7
 {
     public class SudokuSolver
     {
         private readonly SudokuBoard _board;
-        private readonly int _size;
-        private readonly int _boxSize;
-        private int[] rowUsed;
-        private int[] colUsed;
-        private int[] boxUsed;
+        private readonly SolverState _state;
 
         public SudokuSolver(SudokuBoard board)
         {
             _board = board;
-            _size = board.GetSize();
-            _boxSize = board.GetBoxSize();
-            rowUsed = new int[_size];
-            colUsed = new int[_size];
-            boxUsed = new int[_size];
-            Initialize();
+            _state = new SolverState(board);
         }
 
-        private void Initialize()
-        {
-            for (int r = 0; r < _size; r++)
-            {
-                for (int c = 0; c < _size; c++)
-                {
-                    int val = _board.GetCellValue(r, c);
-                    if (val != 0)
-                    {
-                        int mask = 1 << (val - 1);
-                        rowUsed[r] |= mask;
-                        colUsed[c] |= mask;
-                        boxUsed[SolutionHandler.GetBoxIndex(r, c, _boxSize)] |= mask;
-                    }
-                }
-            }
-        }
-
-        public bool Solve()
+        public bool SolveWithBacktracking()
         {
             return Backtrack();
         }
 
         private bool Backtrack()
         {
-            while (ApplyHeuristics()) ;
-            if (SolutionHandler.IsSolved(_board))
+            while (ApplySolvingStrategies()) ;
+
+            if (BoardStateManager.IsSolved(_board))
                 return true;
-            var (selectedRow, selectedCol) = SelectNextCell();
-            if (selectedRow == -1 && selectedCol == -1)
+
+            var (bestCandidateRow, bestCandidateCol) = FindBestCandidateCell();
+            if (bestCandidateRow == -1 && bestCandidateCol == -1)
                 return false;
-            int boxIndexSelected = SolutionHandler.GetBoxIndex(selectedRow, selectedCol, _boxSize);
-            int optionsMaskSelected = ~(rowUsed[selectedRow] | colUsed[selectedCol] | boxUsed[boxIndexSelected]) & ((1 << _size) - 1);
-            for (int val = 1; val <= _size; val++)
+
+            int boxIndex = BoardStateManager.GetBoxIndex(bestCandidateRow, bestCandidateCol, _state.BoxSize);
+            int availableNumbersMask = ~(_state.RowConstraints[bestCandidateRow] |
+                                         _state.ColConstraints[bestCandidateCol] |
+                                         _state.BoxConstraints[boxIndex]) & ((1 << _state.Size) - 1);
+
+            for (int val = 1; val <= _state.Size; val++)
             {
                 int mask = 1 << (val - 1);
-                if ((optionsMaskSelected & mask) != 0)
+                if ((availableNumbersMask & mask) != 0)
                 {
                     SudokuBoard clonedBoard = _board.Clone();
-                    int[] clonedRowUsed = (int[])rowUsed.Clone();
-                    int[] clonedColUsed = (int[])colUsed.Clone();
-                    int[] clonedBoxUsed = (int[])boxUsed.Clone();
-                    SolutionHandler.AssignValue(_board, rowUsed, colUsed, boxUsed, selectedRow, selectedCol, val, _boxSize);
+                    int[] clonedRowConstraints = (int[])_state.RowConstraints.Clone();
+                    int[] clonedColConstraints = (int[])_state.ColConstraints.Clone();
+                    int[] clonedBoxConstraints = (int[])_state.BoxConstraints.Clone();
+
+                    BoardStateManager.PlaceNumber(_board, _state.RowConstraints, _state.ColConstraints, _state.BoxConstraints,
+                                                  bestCandidateRow, bestCandidateCol, val, _state.BoxSize);
+
                     bool result = Backtrack();
                     if (result)
                         return true;
-                    SolutionHandler.RestoreState(_board, clonedBoard, rowUsed, clonedRowUsed, colUsed, clonedColUsed, boxUsed, clonedBoxUsed);
+
+                    BoardStateManager.UndoMove(_board, clonedBoard, _state.RowConstraints, clonedRowConstraints,
+                                               _state.ColConstraints, clonedColConstraints, _state.BoxConstraints, clonedBoxConstraints);
                 }
             }
+
             return false;
         }
 
-        private bool ApplyHeuristics()
+        private bool ApplySolvingStrategies()
         {
             bool progress = false;
-            progress |= NakedSinglesSolver.ApplyNakedSingles(_board, rowUsed, colUsed, boxUsed, _size, _boxSize);
-            if (_size >= 16)
-                progress |= HiddenSinglesSolver.ApplyHiddenSingles(_board, rowUsed, colUsed, boxUsed, _size, _boxSize);
+            progress |= NakedSinglesSolver.ApplyNakedSingles(_board, _state.RowConstraints, _state.ColConstraints, _state.BoxConstraints, _state.Size, _state.BoxSize);
+            if (_state.Size >= 16)
+            {
+                progress |= HiddenSinglesSolver.ApplyHiddenSingles(_board, _state.RowConstraints, _state.ColConstraints, _state.BoxConstraints, _state.Size, _state.BoxSize);
+            }
             return progress;
         }
 
-        private (int row, int col) SelectNextCell()
+        private (int row, int col) FindBestCandidateCell()
         {
-            int minOptions = _size + 1;
-            int selectedRow = -1;
-            int selectedCol = -1;
+            int minOptions = _state.Size + 1;
+            int bestCandidateRow = -1;
+            int bestCandidateCol = -1;
             int highestDegree = -1;
             bool foundSingleOption = false;
-            for (int r = 0; r < _size && !foundSingleOption; r++)
+
+            for (int r = 0; r < _state.Size; r++)
             {
-                for (int c = 0; c < _size && !foundSingleOption; c++)
+                foreach (int c in _state.EmptyCellsInRow[r])
                 {
                     if (_board.GetCellValue(r, c) != 0)
                         continue;
-                    int usedMask = rowUsed[r] | colUsed[c] | boxUsed[SolutionHandler.GetBoxIndex(r, c, _boxSize)];
-                    int options = ~usedMask & ((1 << _size) - 1);
-                    int optionCount = SolutionHandler.PopCount(options);
-                    if (optionCount < minOptions)
+
+                    int usedMask = _state.RowConstraints[r] | _state.ColConstraints[c] | _state.BoxConstraints[BoardStateManager.GetBoxIndex(r, c, _state.BoxSize)];
+                    int options = ~usedMask & ((1 << _state.Size) - 1);
+                    int optionCount = BoardStateManager.PopCount(options);
+
+                    int degree = _state.EmptyCellsInRow[r].Count + _state.EmptyCellsInCol[c].Count + _state.EmptyCellsInBox[BoardStateManager.GetBoxIndex(r, c, _state.BoxSize)].Count;
+
+                    if (optionCount < minOptions || (optionCount == minOptions && degree > highestDegree))
                     {
                         minOptions = optionCount;
-                        selectedRow = r;
-                        selectedCol = c;
-                        highestDegree = CalculateDegree(r, c);
+                        bestCandidateRow = r;
+                        bestCandidateCol = c;
+                        highestDegree = degree;
                         if (optionCount == 1)
                             foundSingleOption = true;
                     }
-                    else if (optionCount == minOptions)
-                    {
-                        int degree = CalculateDegree(r, c);
-                        if (degree > highestDegree)
-                        {
-                            selectedRow = r;
-                            selectedCol = c;
-                            highestDegree = degree;
-                            if (optionCount == 1)
-                                foundSingleOption = true;
-                        }
-                    }
                 }
             }
-            return (selectedRow, selectedCol);
-        }
 
-        private int CalculateDegree(int row, int col)
-        {
-            int degree = 0;
-            for (int c = 0; c < _size; c++)
-            {
-                if (c != col && _board.GetCellValue(row, c) == 0)
-                    degree++;
-            }
-            for (int r = 0; r < _size; r++)
-            {
-                if (r != row && _board.GetCellValue(r, col) == 0)
-                    degree++;
-            }
-            int boxIndex = SolutionHandler.GetBoxIndex(row, col, _boxSize);
-            int startRowBox = (boxIndex / _boxSize) * _boxSize;
-            int startColBox = (boxIndex % _boxSize) * _boxSize;
-            for (int rr = 0; rr < _boxSize; rr++)
-            {
-                for (int cc = 0; cc < _boxSize; cc++)
-                {
-                    int currentRow = startRowBox + rr;
-                    int currentCol = startColBox + cc;
-                    if ((currentRow != row || currentCol != col) && _board.GetCellValue(currentRow, currentCol) == 0)
-                        degree++;
-                }
-            }
-            return degree;
+            return (bestCandidateRow, bestCandidateCol);
         }
     }
 }
-
