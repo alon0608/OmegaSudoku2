30bf7dce01f41d4c7a0d837cf121966a86ded182
alon0608 <alonsh0608@gmail.com>
2025-02-11 00:31:59 +0200

    added comments to hidden singles class

--- Solvers/HiddenSinglesSolver.cs
+++ Solvers/HiddenSinglesSolver.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace SudokuOmega7
 {
+    /// <summary>
+    /// Implements the Hidden Singles solving technique for Sudoku.
+    /// This strategy identifies cells where a number can only appear once in a row, column, or box.
+    /// </summary>
     public static class HiddenSinglesSolver
     {
+        /// <summary>
+        /// Applies the Hidden Singles strategy to the entire Sudoku board.
+        /// A Hidden Single occurs when a number has only one possible position in a given row, column, or box.
+        /// </summary>
+        /// <param name="board">The Sudoku board.</param>
+        /// <param name="rowUsed">Bitmasks tracking used numbers in each row.</param>
+        /// <param name="colUsed">Bitmasks tracking used numbers in each column.</param>
+        /// <param name="boxUsed">Bitmasks tracking used numbers in each box.</param>
+        /// <param name="size">The size of the Sudoku board.</param>
+        /// <param name="boxSize">The size of a Sudoku box.</param>
+        /// <returns>True if at least one Hidden Single was found and placed, otherwise false.</returns>
         public static bool ApplyHiddenSingles(
             SudokuBoard board,
             int[] rowUsed,
@@ -23,6 +38,17 @@ namespace SudokuOmega7
             return progress;
         }
 
+        /// <summary>
+        /// Searches for Hidden Singles in a specific unit and places them if found.
+        /// </summary>
+        /// <param name="board">The Sudoku board.</param>
+        /// <param name="rowUsed">Bitmasks for row constraints.</param>
+        /// <param name="colUsed">Bitmasks for column constraints.</param>
+        /// <param name="boxUsed">Bitmasks for box constraints.</param>
+        /// <param name="size">The size of the board.</param>
+        /// <param name="boxSize">The size of each box.</param>
+        /// <param name="unitType">The type of unit to analyze (row, column, or box).</param>
+        /// <returns>True if any numbers were placed, otherwise false.</returns>
         private static bool ApplyHiddenSinglesForUnit(
             SudokuBoard board,
             int[] rowUsed,
@@ -40,13 +66,17 @@ namespace SudokuOmega7
                 {
                     valueToCells[i] = new List<(int, int)>();
                 }
+
+                // Get all the cells in the current unit (row, column, or box)
                 List<(int row, int col)> cells = GetUnitCells(unitType, unit, size, boxSize);
-                foreach (var (r, c) in cells)
+                foreach ((int r, int c) in cells)
                 {
                     if (board.GetCellValue(r, c) != 0)
                         continue;
+
                     int usedMask = rowUsed[r] | colUsed[c] | boxUsed[BoardStateManager.GetBoxIndex(r, c, boxSize)];
                     int options = ~usedMask & ((1 << size) - 1);
+
                     for (int val = 1; val <= size; val++)
                     {
                         if ((options & (1 << (val - 1))) != 0)
@@ -55,13 +85,15 @@ namespace SudokuOmega7
                         }
                     }
                 }
-                foreach (var kvp in valueToCells)
+
+                // Place numbers if a Hidden Single is found
+                foreach (KeyValuePair<int, List<(int row, int col)>> kvp in valueToCells)
                 {
                     int val = kvp.Key;
                     List<(int row, int col)> possibleCells = kvp.Value;
                     if (possibleCells.Count == 1)
                     {
-                        var (r, c) = possibleCells[0];
+                        (int r, int c) = possibleCells[0];
                         if (board.GetCellValue(r, c) == 0)
                         {
                             BoardStateManager.PlaceNumber(board, rowUsed, colUsed, boxUsed, r, c, val, boxSize);
@@ -73,6 +105,14 @@ namespace SudokuOmega7
             return progress;
         }
 
+        /// <summary>
+        /// Retrieves all cells in a given row, column, or box.
+        /// </summary>
+        /// <param name="unitType">The type of unit.</param>
+        /// <param name="unit">The index of the unit.</param>
+        /// <param name="size">The size of the Sudoku board.</param>
+        /// <param name="boxSize">The size of each box.</param>
+        /// <returns>A list of cell coordinates within the specified unit.</returns>
         private static List<(int row, int col)> GetUnitCells(UnitType unitType, int unit, int size, int boxSize)
         {
             List<(int row, int col)> cells = new List<(int row, int col)>();
@@ -101,6 +141,9 @@ namespace SudokuOmega7
             return cells;
         }
 
+        /// <summary>
+        /// Represents the different unit types in Sudoku (row, column, and box).
+        /// </summary>
         private enum UnitType
         {
             Row,
